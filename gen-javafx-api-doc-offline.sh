d072ad612fb3b9c745b97f1ee4591719f82b7ea1
Kid <kid.in.gnu@gmail.com>
2025-10-26 03:38:18 +0800

    add javadoc practising script

---
+++ gen-javafx-api-doc-offline.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## purpose: practising javadoc cli, generate the javadoc offline
+## tested in git bash ONLY
+
+PREFIX=jfx
+VERSION=21.0.9
+SOURCE="${PREFIX}-${VERSION}"
+OUTPUT="${PREFIX}-${VERSION}_api"
+
+mkdir $SOURCE $OUTPUT
+
+## src.zip here is a zipped file downloaded from https://gluonhq.com/products/javafx/, javafx
+unzip src.zip -d $SOURCE
+
+MODULES=`ls $PREFIX-$VERSION`
+MODULE_OPTIONS=
+
+for m in $MODULES
+do
+    MODULE_OPTIONS="${MODULE_OPTIONS} --module ${m}"
+done
+
+STR="javadoc -d $OUTPUT --module-source-path $SOURCE $MODULE_OPTIONS -tag moduleGraph,implSpec"
+
+## e.g. javadoc -d jfx-21.0.9_API --module-source-path jfx-21.0.9 --module javafx.base --module javafx.controls --module javafx.fxml --module javafx.graphics --module javafx.media --module javafx.swing --module javafx.web -tag moduleGraph,implSpec
+# skipped the custom tags for not generating the errors
+javadoc -d $OUTPUT --module-source-path $SOURCE $MODULE_OPTIONS -tag moduleGraph,implSpec
